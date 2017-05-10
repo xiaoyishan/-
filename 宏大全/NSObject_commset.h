441a4d8f18f85e1961b5bba3802176c84ccb4d88
xiaoyishan <xiaoyishan1992@163.com>
2017-05-10 14:30:14 +0800

    first commit

---
+++ 宏大全/NSObject_commset.h
@@ -0,0 +1,13 @@
+//
+//  NSObject_commset.h
+//  宏大全
+//
+//  Created by Sundear on 2017/5/8.
+//  Copyright © 2017年 xiexin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject ()
+
+@end
