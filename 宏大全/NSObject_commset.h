c91a1ed5f7944fce104aa7e5ac8a9cf09bc9afdb
xiaoyishan <xiaoyishan1992@163.com>
2017-10-26 18:27:58 +0800

    add new
    RandomColor

--- 宏大全/NSObject_commset.h
+++ 宏大全/NSObject_commset.h
@@ -23,6 +23,7 @@
 //颜色
 #define kColor_RGB(r,g,b)       [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:1.0]
 #define kColor_RGBA(r,g,b,a)    [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:(a)]
+#define RandomColor UIColorRGB(arc4random_uniform(256), arc4random_uniform(256), arc4random_uniform(256))
 
 //定义字号
 #define kFont(X)                [UIFont systemFontSize:X]
